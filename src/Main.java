2fb504353c462a40263ae33eb95bbc1aaa498235
Ramil Burgart <ramilburgart7006@gmail.com>
2023-07-14 21:34:16 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 import java.io.File;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+
         String folderPath = "C:/Users/babol/Innopolis";
         File file = new File(folderPath);
 
-        System.out.println(file.length());
-        System.out.println(System.getProperties().get("user.dir"));
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
     public static long getFolderSize(File folder){
         if(folder.isFile()){
